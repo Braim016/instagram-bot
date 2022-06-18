5cfd19b3fd78ff559d4b560e9ded405f0e183724
Braim016 <103691650+Braim016@users.noreply.github.com>
2022-06-18 14:53:02 +0100

    first commit

--- instagram_bot/constants.py
+++ instagram_bot/constants.py
@@ -1,24 +1,4 @@
-import logging
 import sys
-import time
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s : %(name)s : %(levelname)s : %(message)s', datefmt='%d/%m/%y %H:%M:%S')
-
-file_handler = logging.FileHandler('file.log')
-file_handler.setLevel(logging.ERROR)
-file_handler.setFormatter(formatter)
-
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
-
-# Starting the timer
-start_time = time.perf_counter()
 
 instagram_site = 'https://www.instagram.com'
 
